704f1705997a226354ac0cd96d04ebeb7c9bef73
AnnaKim <annakim@inbox.ru>
2023-06-01 19:39:23 +0300

    changed

--- src/main/java/sem3/task1.java
+++ src/main/java/sem3/task1.java
@@ -12,19 +12,20 @@ public class task1 {
     static List<Integer> list1 = new ArrayList<>();
 
     public static void main(String[] args) {
-        for (int i = 0; i < 20; i++) {
+        for (int i = 0; i < 10; i++) {
             list1.add(rndList.nextInt(20));
         }
-        System.out.println(list1);
+         System.out.println(list1);
         System.out.println(Collections.min(list1));
-        System.out.println(Collections.max(list1));
-        for (int i = 0; i < list1.size(); i++) {
+                for (int i = 0; i < list1.size(); i++) {
             if (list1.get(i) % 2 == 0) {
                 list1.remove(i);
                 i--;
             }
         }
         System.out.println(list1);
+
+        // вывод среденего значения
         int sum = 0;
         for (int i = 0; i < list1.size(); i++){
             int num = list1.get(i);
@@ -35,9 +36,3 @@ public class task1 {
     }
 
 }
-
-
-
-
-
-
