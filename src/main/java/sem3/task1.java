c6f8327da090cd8493635e61ca2f79a4b8a489f3
AnnaKim <annakim@inbox.ru>
2023-06-01 19:18:09 +0300

    task1

---
+++ src/main/java/sem3/task1.java
@@ -0,0 +1,43 @@
+//Пусть дан произвольный список целых чисел.
+//
+//1) Нужно удалить из него чётные числа
+//2) Найти минимальное значение
+//3) Найти максимальное значение
+//4) Найти среднее значение
+package sem3;
+import java.util.ArrayList;
+import java.util.*;
+public class task1 {
+    static Random rndList = new Random();
+    static List<Integer> list1 = new ArrayList<>();
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 20; i++) {
+            list1.add(rndList.nextInt(20));
+        }
+        System.out.println(list1);
+        System.out.println(Collections.min(list1));
+        System.out.println(Collections.max(list1));
+        for (int i = 0; i < list1.size(); i++) {
+            if (list1.get(i) % 2 == 0) {
+                list1.remove(i);
+                i--;
+            }
+        }
+        System.out.println(list1);
+        int sum = 0;
+        for (int i = 0; i < list1.size(); i++){
+            int num = list1.get(i);
+            sum += num;
+        }
+        double avg = (double) sum / list1.size();
+        System.out.println(avg);
+    }
+
+}
+
+
+
+
+
+
